f95ec9a7da81cd61e6f01ada18d80342b5eefdff
proxyfil <piloupilou200@gmail.com>
2024-09-06 14:47:53 +0000

    auto : updated config-2024.js 1725634073579

--- js/configs/config-2024.js
+++ js/configs/config-2024.js
@@ -10,25 +10,5 @@ const canvasCenter = {
 }
 window.canvasCenter = canvasCenter
 
-let variationsConfig = {
-	"zevent2024": {
-		"name": "ZEvent2024 Place",
-		"code": "",
-		"default": 0,
-		"drawablePeriods": [
-			0,
-			0
-		],
-		"expansions": [],
-		"versions" : [
-			{
-				"timestamp": 1725639000,
-				"url": [
-					"./img/canvas/default/1662737400.png"
-				]
-			}
-		],
-		"icon": "<svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='currentColor' viewBox='0 0 11 11' aria-hidden='true'><polygon points='3 0 8 0 8 1 9 1 9 2 10 2 10 3 9 3 9 4 10 4 10 3 11 3 11 4 11 8 10 8 10 9 9 9 9 8 8 8 8 7 7 7 7 5 8 5 8 4 7 4 7 3 6 3 6 2 7 2 7 1 5 1 5 2 4 2 4 5 5 5 5 6 3 6 3 5 1 5 1 7 3 7 3 8 4 8 4 10 8 10 8 11 3 11 3 10 2 10 2 9 1 9 1 8 0 8 0 3 1 3 1 2 2 2 2 1 3 1 3 0'/><polygon points='8 9 8 10 9 10 9 9'/></svg>"
-	}
-}
+let variationsConfig = {"zevent2024":{"name":"ZEvent2024 Place","code":"","default":1,"drawablePeriods":[0,1],"expansions":[],"versions":[{"timestamp":1725639000,"url":["./img/canvas/default/1662737400.png"]}],"icon":"<svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='currentColor' viewBox='0 0 11 11' aria-hidden='true'><polygon points='3 0 8 0 8 1 9 1 9 2 10 2 10 3 9 3 9 4 10 4 10 3 11 3 11 4 11 8 10 8 10 9 9 9 9 8 8 8 8 7 7 7 7 5 8 5 8 4 7 4 7 3 6 3 6 2 7 2 7 1 5 1 5 2 4 2 4 5 5 5 5 6 3 6 3 5 1 5 1 7 3 7 3 8 4 8 4 10 8 10 8 11 3 11 3 10 2 10 2 9 1 9 1 8 0 8 0 3 1 3 1 2 2 2 2 1 3 1 3 0'/><polygon points='8 9 8 10 9 10 9 9'/></svg>"}}
 window.variationsConfig = variationsConfig
